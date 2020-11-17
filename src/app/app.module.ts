1f7d196a2782013fee72898be2611fc0409429a8
dberry <dberry@synergentcorp.com>
2020-11-17 14:42:53 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { WavesModule, ButtonsModule } from 'ng-uikit-pro-standard';
 
 import { AppComponent } from './app.component';
+import { TableEditComponent } from './components/table-edit/table-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableEditComponent
   ],
   imports: [
     BrowserModule
